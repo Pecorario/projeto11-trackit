9a6d61be3a39744ee8f1ee24b66c74c6e94ac5b7
Taynara Pecorario <taynara_rp22@hotmail.com>
2023-03-14 02:53:16 -0300

    :sparkles: basic structure project added

---
+++ src/pages/Login/index.js
@@ -0,0 +1,11 @@
+import * as S from './style';
+
+const Login = () => {
+  return (
+    <S.Container>
+      <h1>TrackIt</h1>
+    </S.Container>
+  );
+};
+
+export default Login;
---
+++ src/style/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  colors: {
+    white: '#ffffff',
+    darkBlue: '#126BA5',
+    lightBlue: '#52B6FF',
+    darkGray: '#CFCFCF',
+    lightGray: '#D4D4D4',
+    lighterGray: '#E7E7E7',
+    background: '#F2F2F2',
+    textDark: '#666666',
+    textLight: '#DBDBDB',
+    green: '#8FC549'
+  }
+});
+
+export default theme;
